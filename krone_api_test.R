6c372acbf80f0a4c9e9ead5762c314f98de29d18
Robert Kalcik <robert_kck@hotmail.com>
2018-04-07 13:45:59 +0200

    improved krone api downloader

--- krone_api_test.R
+++ krone_api_test.R
@@ -2,18 +2,29 @@ library(jsonlite)
 library(tidyverse)
 library(rvest)
 
-krone <- read_html("http://www.krone.at/welt")
-articles <- krone %>% html_nodes(".imgteaser")
+c = NULL
+for (r in c("politik", "welt", "sport")) {
+  print(r)
+  krone <- read_html(paste0("http://www.krone.at/", r))
+  articles <- krone %>% html_nodes(".imgteaser")
 
-# articles %>%
-#   map(html_nodes, ".l_title") %>%
-#   map(html_text, trim = TRUE)
+  # articles %>%
+  #   map(html_nodes, ".l_title") %>%
+  #   map(html_text, trim = TRUE)
 
-links <- tibble(
-  # title = articles %>% html_nodes(".l_title") %>% html_text(trim = TRUE),
-  url = articles %>% html_nodes("a") %>% html_attr("href"),
-  id_article = url %>% str_extract("[:digit:]+")
-)
+  links <- tibble(
+    # title = articles %>% html_nodes(".l_title") %>% html_text(trim = TRUE),
+    title = articles %>% html_nodes("a") %>% html_text(trim = TRUE),
+    url = articles %>% html_nodes("a") %>% html_attr("href"),
+    id_article = url %>% str_extract("[:digit:]+")
+  )
+  df <- map_df(links$id_article, parse_article) %>% mutate(
+    r = r
+  )
+  c <- rbind(c, df)
+}
+
+save(c, file = "scrape.RData")
 
 parse_article <- function(id_article) {
   j = 2
@@ -42,8 +53,8 @@ parse_article <- function(id_article) {
     )
 }
 
-c <- map_df(links$id_article, parse_article)
-unnest(what)
+
+
 
 comments_json <- fromJSON("http://api.krone.at/v1/comments/posts/1687051/page/1?limit=10&cors=true&domain=www.krone.at",
                           flatten = TRUE)
