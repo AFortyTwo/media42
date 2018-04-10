4835831df53126ea2f851d235119a4a2b228f152
Robert Kalcik <robert_kck@hotmail.com>
2018-04-10 18:42:58 +0200

    second scrape, moving results to the export folder

--- krone_api_test.R
+++ krone_api_test.R
@@ -2,21 +2,30 @@ library(jsonlite)
 library(tidyverse)
 library(rvest)
 
+# Resortliste aus krone.at/ URL
 df <- tibble(resort = c("bundeslaender", "wirtschaft",  "politik", "welt", "sport", "oesterreich",
                         "nachrichten", "wissen", "viral", "fussball", "motorsport", "wintersport",
                         "tennis", "stars-society", "lifestyle", "kino", "musik", "medien", "digital",
                         "freizeit", "auto", "web", "elektronik", "spiele", "digitale-trends",
                         "gesund-fit", "life", "tierecke", "reisen-urlaub", "bauen-wohnen", "lieblingsrezept"
                         ))
+
+# Download links zu den Artikel für jede Resortseite und bereinigen
 df <- df %>% mutate(links = map(resort, get_links)) %>% unnest()
 df <- df %>%
   mutate(n = TRUE, resort = str_replace(resort, "-", "_")) %>%
   spread(resort, n, fill = FALSE)
+
+# Alle Kommentare inklusive Diggs runterladen (Dauer ca. XX) und bereinigen
+tictoc::tic()
 df_comments <- df %>% mutate(comments = map(id_article, get_comments))
+tictoc::toc()
 df_comments <- df_comments %>% filter(comments != "no comments") %>% unnest()
 
-save(df_comments, file = "scrape.RData")
-df_comments %>% write_csv(paste0("krone_scrape_", Sys.Date(),".csv"))
+# Speichern unter heutigem Datum
+save(df_comments, file = paste0("export/scrape_", Sys.Date(), ".RData"))
+df_comments %>% write_csv(paste0("export/krone_scrape_", Sys.Date(),".csv"))
+
 
 
 
